ec4c4e4b75fc37bb13e7972b891e43cbaf00e80e
lobinhodev <contatolobinhodev@gmail.com>
2023-04-30 16:23:23 -0300

    init: commit initial

---
+++ Conditionals/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Digit your note: ");
+var readNote = Console.ReadLine();
+var note = int.Parse(readNote);
+
+if (note >= 70)
+{
+    Console.WriteLine("Approval");
+}
+else if (note >= 40)
+{
+    Console.WriteLine("Recuperation");
+}
+else
+{
+    Console.WriteLine("Preapproval");
+}
