42d9e4177c9607e934e4d8aea8bedba9d7dfb9a9
lobinhodev <contatolobinhodev@gmail.com>
2023-04-30 16:33:33 -0300

    feat: add switch case

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -4,6 +4,8 @@ Console.WriteLine("Digit your note: ");
 var readNote = Console.ReadLine();
 var note = int.Parse(readNote);
 
+// if / else
+
 if (note >= 70)
 {
     Console.WriteLine("Approval");
@@ -15,4 +17,43 @@ else if (note >= 40)
 else
 {
     Console.WriteLine("Preapproval");
+}
+
+// switch / case
+Console.WriteLine("Welcome to company x");
+
+Console.WriteLine("Digit 1 - Contraction to plan");
+Console.WriteLine("Digit 2 - FAQ");
+Console.WriteLine("Digit 3 - Reclamation");
+Console.WriteLine("Digit 4 - Duplicate ticket");
+
+var option = int.Parse(Console.ReadLine());
+
+switch (option)
+{
+    case 1:
+    {
+        Console.WriteLine("Information to plan.");
+        break;
+    }
+    case 2:
+    {
+        Console.WriteLine("Answer to question.");
+        break;
+    }
+    case 3:
+    {
+        Console.WriteLine("Talk do reclamation.");
+        break;
+    }
+    case 4:
+    {
+        Console.WriteLine("Verify your e-mail.");
+        break;
+    }
+    default:
+    {
+        Console.WriteLine("Option not found.");
+        break;
+    }
 }
